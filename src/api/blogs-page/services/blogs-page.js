bb762cc7f7f31b2ea596c0ad734c2ba6344d0720
fasluolassery <fasluolassery@gmail.com>
2026-05-18 17:31:56 +0530

    fix(backend): expose CMS page routes

--- src/api/blogs-page/services/blogs-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * blogs-page service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::blogs-page.blogs-page');
